2bc39c99a09b76ff572ccea6b25dc2bf94a42b8b
kunalkhakhar-continuum <kkhakhar-continuum@users.noreply.github.com>
2018-10-24 02:41:11 +0530

    create a changed package

--- random.js
+++ random.js
@@ -1,19 +1,71 @@
 const path = require('path');
+const log = (message) => {
+    console.log('*****************');
+    console.log(message);
+};
 var execSync = require('child_process').execSync;
 const exec = (command) => execSync(command, { encoding: 'utf8' }).trim();
 const LERNA_BIN = path.join(exec('npm bin'), 'lerna');
-console.log(`LERNA_BIN: ${LERNA_BIN}`);
-const lerna = (args) => exec(`${LERNA_BIN} ${args}`);
+log(`LERNA_BIN: ${LERNA_BIN}`);
+const lerna = (args) => { 
+    log(`executing lerna command: ${LERNA_BIN} ${args}`);
+    return exec(`${LERNA_BIN} ${args}`);
+}
 
 const split = (pattern) => (chars) => chars.split(pattern);
 const zip = (destination, source) => { 
-    console.log(`destination: ${destination}`);
+    log(`destination: ${destination}`);
     return Object.assign(destination, { [source[0]]: source[1] })
 };
 
 const isPkg = (tag) => tag.match(/[\w-]+@\d+\.\d+\.\d+(-[\w\d]+)?/);
 
-console.log(lerna('updated'));
+const checkIfAnyPackageUpdated = (output) => {
+    const stdout = output.split('\n').reverse();
+    if (stdout[0] === 'lerna info No changed packages found') {
+        log(stdout[0]);
+        return false;
+    }
+
+    return true;
+}
+
+const isEndofPackageList = (line) => line.contains('lastTagName:');
+
+const getChangedPackages = (output) => {
+    const stdout = output.split('\n').reverse();
+    let i = 0;
+    const packages = [];
+    while(!isEndofPackageList(stdout[i])) {
+        packages.push(stdout[i]);
+        i++;
+    }
+
+    return packages;
+}
+
+buildAllPackages = () => {
+    log('building all packages');
+}
+
+let changedPackages;
+const allManagedPackages = lerna('ls'); // list all managed packages
+log('allManagedPackages');
+log(allManagedPackages);
+
+const output = lerna('changed'); // get only the packages that have updated since last tag
+
+// if (checkIfAnyPackageUpdated(output)) {
+//     log(output);
+//     changedPackages = getChangedPackages(output);
+// } else {
+//     log(output);
+// }
+
+// if (changedPackages.length === 0) {
+//     log('No changed packaged are found, will rebuild all packages')
+//     buildAllPackages()
+// }
 
 // const updatedPackages = exec('git tag --list --contain HEAD')
 //   .split('\n')
