77c6fbcc852d7b39c2164c9cf4dad0b05a72fc30
kunalkhakhar-continuum <kkhakhar-continuum@users.noreply.github.com>
2018-10-24 15:06:44 +0530

    commit everything

--- packages/shared/scripts/prepare.js
+++ packages/shared/scripts/prepare.js
@@ -3,6 +3,6 @@ const random = require('../../../random.js');
 var exec = random.exec;
 const env = random.NODE_ENV;
 const WEBPACK_BIN = path.join(exec('npm bin'), 'webpack');
-
+console.log(`WEBPACK_BIN: ${WEBPACK_BIN}`);
 // cross-env NODE_ENV=dt webpack --config webpack/webpack.rmm-its-portal.app.config.js --progress --profile --colors
 exec(`cross-env NODE_ENV=${env} ${WEBPACK_BIN} --config ../webpack/webpack.config.js --progress --profile --colors`);
