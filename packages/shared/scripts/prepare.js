125198076f1d12c0a428ce1421b0fe33fb8cc838
kunalkhakhar-continuum <kkhakhar-continuum@users.noreply.github.com>
2018-10-24 16:57:15 +0530

    trigger lerna publish

--- packages/shared/scripts/prepare.js
+++ packages/shared/scripts/prepare.js
@@ -5,6 +5,6 @@ const exec = require('../../../deploy-utils.js').exec;
 console.log(`current env: ${NODE_ENV}`)
 const WEBPACK_BIN = path.join(exec('npm bin'), 'webpack');
 console.log(`WEBPACK_BIN: ${WEBPACK_BIN}`);
-// run: cross-env NODE_ENV=dt webpack --config webpack/webpack.rmm-its-portal.app.config.js --progress --profile --colors
+// run: cross-env NODE_ENV=dt webpack --config webpack/webpack.rmm-its-portal.app.config.js --progress --profile --colors 
 // exec(`cross-env NODE_ENV=${NODE_ENV} ${WEBPACK_BIN} --config ../webpack/webpack.config.js --progress --profile --colors`);
 exec(`cross-env NODE_ENV=${NODE_ENV} node ../build_dt_works.js`);
