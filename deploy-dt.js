efec8657a5758a5c2c60e63ad53acd1b19436afd
kunalkhakhar-continuum <kkhakhar-continuum@users.noreply.github.com>
2018-10-25 10:12:04 +0530

    invoke package deploy

--- deploy-dt.js
+++ deploy-dt.js
@@ -1,4 +1,4 @@
 const deployProps = require('./deploy-utils.js')
 const deployPackages = require('./deploy-packages.js');
 console.log(`deploy-dt ${process.env.NODE_ENV}`);
-// deployPackages().deploy();
+deployPackages().deploy();
