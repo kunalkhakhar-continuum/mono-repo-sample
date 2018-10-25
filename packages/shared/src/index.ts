99410449c9b2883d9fddd09ff26d062cb394cb0a
kunalkhakhar-continuum <kkhakhar-continuum@users.noreply.github.com>
2018-10-25 20:10:59 +0530

    lerna changed when one package version is prerelease and a package with major has commit

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -2,3 +2,4 @@ export { Header } from './components/header';
 export { getTestTheme } from './components/test-support/get-test-theme';
 
 // test: lerna changed when shared package is updated
+// test: lerna changed when one package version is prerelease and a package with major has commit
