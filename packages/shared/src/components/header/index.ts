a626a7176d813956fd4103eaadb942f388ae6c7e
kunalkhakhar-continuum <kkhakhar-continuum@users.noreply.github.com>
2018-10-25 15:46:14 +0530

    test canary

--- packages/shared/src/components/header/index.ts
+++ packages/shared/src/components/header/index.ts
@@ -1,4 +1,4 @@
 export { Header } from './header';
 // check for process.env variable again try lerna canary after a failed publush
 // check branch is behind before lerna publish
-// check with canary
+// check with canary again
