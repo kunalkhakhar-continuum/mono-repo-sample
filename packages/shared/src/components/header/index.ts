eb01d723ee5d015b0f06352a069e819ce3774dc3
kunalkhakhar-continuum <kkhakhar-continuum@users.noreply.github.com>
2018-10-25 14:36:55 +0530

    make the publish behind in local branch

--- packages/shared/src/components/header/index.ts
+++ packages/shared/src/components/header/index.ts
@@ -1,3 +1,4 @@
 export { Header } from './header';
 // check for process.env variable again try lerna canary after a failed publush
 // check branch is behind before lerna publish
+// commit to make publish behind
